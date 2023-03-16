a552e432a044b6b916598f0e1313223439238890
hadeelengineer <hadeelalwada@gmail.com>
2023-03-16 08:10:28 +0200

    built in8.js folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/header';
-import Translation from './components/header/translation';
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Translation/>
-      
+    
     </div>
   );
 }
--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -4,7 +4,6 @@ import Backend from "i18next-http-backend";
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 
-
 i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
 
     fallbackLng: "en",
@@ -13,6 +12,7 @@ i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
     },
   });
 
+  
 export default i18n;
 
 
