438ab2a9f963ecaa0edf17a3e770208f410be2e1
hadeelengineer <hadeelalwada@gmail.com>
2023-03-20 08:53:27 +0200

    create all pages files in pages folder  which neet in this app

---
+++ src/pages/ActionsOfProducts.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ActionsOfProducts() {
+  return (
+    <div>ActionsOfProducts</div>
+  )
+}
+
+export default ActionsOfProducts
---
+++ src/pages/PlaceOrderProducts.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PlaceOrderProducts = () => {
+  return (
+    <div>PlaceOrderProducts</div>
+  )
+}
+
+export default PlaceOrderProducts
